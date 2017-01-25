cb296991610806c63354a3772e7f854821c57600
Mark <18929562811@163.com>
2017-01-25 16:07:48 +0800

    RAC的next信号简单和完整写法

--- RAC_1/RACSimpleDemo/ViewController.m
+++ RAC_1/RACSimpleDemo/ViewController.m
@@ -23,10 +23,28 @@
     [self.userName_text addTarget:self action:@selector(usernameTextFieldChanged) forControlEvents:UIControlEventEditingChanged];
     [self.passWord_text addTarget:self action:@selector(passwordTextFieldChanged) forControlEvents:UIControlEventEditingChanged];
     //RAC 有三种信号 next error completed 一个signal 因error终结前可以发送任意数量的next事件
-    [self.userName_text.rac_textSignal subscribeNext:^(id x) {
+    // rac_textSignal 是RAC框架通过category为很多基本UIKit控件添加signal，这样你的控件就添加了订阅。
+    //1,第一种写法
+    [[self.userName_text.rac_textSignal
+     
+     filter:^BOOL(id value) {//过滤
+         NSString *text = value;
+         return text.length > 3;
+     }]
+     subscribeNext:^(id x) {//订阅信号
         
         NSLog(@"用户名输入框 %@",x);
-        
+     }];
+    //2,第二种写法
+    //RAC的每个操作都会返回一个RACSignal的信号，这叫连贯接口，你不用每一步使用本地变量
+    RACSignal *usernamesignal = self.userName_text.rac_textSignal;
+    RACSignal *filterUsername = [usernamesignal filter:^BOOL(id value) {
+        NSString *text = value;
+        return  text.length > 3;
+    }];
+    
+    [filterUsername subscribeNext:^(id x) {
+        NSLog(@"用户名输入框 %@",x);
     }];
     
     
