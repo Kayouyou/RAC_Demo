dafeefab7ff2b8d128ab6588d8a732b427d2f600
Mark <18929562811@163.com>
2017-01-31 21:15:33 +0800

    rac学习笔记项目搭建RAC环境

---
+++ RAC_2/RAC_2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RAC_2
+//
+//  Created by 叶杨杨 on 2017/1/31.
+//  Copyright © 2017年 叶杨杨. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RAC_2/RAC_2/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  RAC_2
+//
+//  Created by 叶杨杨 on 2017/1/31.
+//  Copyright © 2017年 叶杨杨. All rights reserved.
+//
+
+#import "ViewController.h"
+//关于RAC的一些小知识点
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
