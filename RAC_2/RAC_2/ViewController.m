a8fbb6be70a60c7a7f6127e652dc00575229cbfa
Mark <18929562811@163.com>
2017-01-31 21:25:07 +0800

    基础部分之textfield

--- RAC_2/RAC_2/ViewController.m
+++ RAC_2/RAC_2/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 //关于RAC的一些小知识点
 
-@interface ViewController ()
+@interface ViewController ()<UITextFieldDelegate>
 
 @end
 
@@ -19,6 +19,35 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
+
+/***** 第一 基础部分 *****/
+#pragma mark - textfield属性变化
+
+- (void)textFieldChange{
+    
+    UITextField *textField = ({
+        UITextField *tetxField = [[UITextField alloc] init];
+        textField.backgroundColor = [UIColor darkGrayColor];
+        tetxField;
+    });
+    
+    textField.delegate = self;
+    [self.view addSubview:textField];
+    @weakify(self);
+    
+    /**
+     RAC内部封装的类，文本内容变化就会执行block内部的代码块
+     @param x 文本框内容
+     */
+    [textField.rac_textSignal subscribeNext:^(id x) {
+        //@strongify(self);
+        NSLog(@"输入框x的值 %@",x);
+    }];
+    
+}
+
+
+
 
 
 - (void)didReceiveMemoryWarning {
